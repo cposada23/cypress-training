5789ad86c4ee544686e49f397413ba70af9da58c
camiloesub <105243064+camiloesub@users.noreply.github.com>
2022-07-02 16:22:21 -0500

    modify workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'google',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+  extends: ['google'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    '@typescript-eslint',
-    'cypress',
-  ],
-  'rules': {
-    'quotes': ['error', 'single'],
+  plugins: ['@typescript-eslint', 'cypress'],
+  rules: {
+    quotes: ['error', 'single'],
     'cypress/no-assigning-return-values': 'error',
     'cypress/no-unnecessary-waiting': 'error',
     'cypress/assertion-before-screenshot': 'warn',
     'cypress/no-force': 'warn',
     'no-unused-vars': 'warn',
     'require-jsdoc': 'warn',
-    'max-len': ['error', {'code': 120}],
+    'max-len': ['error', { code: 120 }],
+    'object-curly-spacing': true,
   },
 };
